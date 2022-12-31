bf115e61893509bf86dfba55402cd31b852a4371
paulpaul168 <github@paulhoeller.at>
2022-12-31 18:02:25 +0100

    fix x-alt-desc typo

--- main.py
+++ main.py
@@ -58,7 +58,8 @@ def write_csv(events: List[DanceEvent], metadata: MetaData, folder: str):
     csv_path = os.path.join(folder, "events.csv")
     with open(csv_path, "w") as csvfile:
         writer = csv.writer(csvfile, delimiter=",")
-        writer.writerow(["Date", "Name", "Description", "Dancing School", "Website"])
+        writer.writerow(["Date", "Name", "Description",
+                        "Dancing School", "Website"])
         for event in events:
             writer.writerow(
                 [
@@ -139,7 +140,7 @@ def write_ics(events: List[DanceEvent], metadata: MetaData, folder: str):
         ics_event.add("description", event.website + "\n" + event.description)
         ics_event.add(
             "x-alt-desc",
-            """<a href="http: // """
+            """<a href=""""
             + event.website
             + """">Website</a><br><br>"""
             + event.description,
