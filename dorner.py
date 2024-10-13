aa44cc0b38a09e348882ba354bdbf4f917954bfe
paulpaul168 <github@paulhoeller.at>
2024-10-14 01:16:07 +0200

    Better description for dorner

--- dorner.py
+++ dorner.py
@@ -21,7 +21,7 @@ def create_perfections() -> list[DanceEvent]:
                 ends_at=dateparser.parse("22:15"),
                 name="Perfektion",
                 price_euro_cent=700,
-                description="Freitagsperfektion I TanzZeit",
+                description="Favoritenstraße 20, 1040 Wien\nFreitagsperfektion TanzZeit\nIm Dorner Club inkludiert I Dorner Schüler:innen € 5,-- I Gäste € 7,-",
                 dancing_school="Dorner",
                 website="https://tanzdorner.at/#perfektion",
             ),
